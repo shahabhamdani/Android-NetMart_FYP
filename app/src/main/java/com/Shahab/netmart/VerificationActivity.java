762f40a79a28c5507b29196482337009273fdb72
shahabhamdani <shahabhamdani@users.noreply.github.com>
2021-02-05 15:58:06 +0500

    Minor tweaks

--- app/src/main/java/com/Shahab/netmart/VerificationActivity.java
+++ app/src/main/java/com/Shahab/netmart/VerificationActivity.java
@@ -5,13 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class VerificationActivity extends AppCompatActivity {
+
+
+    private EditText emialEt;
+    private Button recoverBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verification);
+
+
+        emialEt = findViewById(R.id.emailEt);
+        recoverBtn = findViewById(R.id.recoverBtn);
+
+
     }
 
     public void onClickRecover(View v){
