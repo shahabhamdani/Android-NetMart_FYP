abc637ea947ab1c84c81d725582a8dd2f130241e
shahabhamdani <shahabhamdani@users.noreply.github.com>
2021-02-05 12:22:35 +0500

    Minor tweaks

--- app/src/main/java/com/Shahab/netmart/RegisterMenuActivity.java
+++ app/src/main/java/com/Shahab/netmart/RegisterMenuActivity.java
@@ -2,13 +2,49 @@ package com.Shahab.netmart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterMenuActivity extends AppCompatActivity {
+
+
+    private TextView txtBuyer;
+    private TextView txtSeller;
+    private TextView txtRider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_menu);
+
+        txtBuyer = findViewById(R.id.txtBuyer);
+        txtSeller = findViewById(R.id.txtSeller);
+        txtRider =  findViewById((R.id.txtRider));
+
+        txtBuyer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity( new Intent(RegisterMenuActivity.this, RegisterUserActivity.class ));
+            }
+        });
+
+        txtSeller.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity( new Intent(RegisterMenuActivity.this, RegisterSellerActivity.class ));
+            }
+        });
+
+        txtRider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity( new Intent(RegisterMenuActivity.this, RegisterRider.class ));
+            }
+        });
     }
 }
--- app/src/main/java/com/Shahab/netmart/RegisterSellerActivity.java
+++ app/src/main/java/com/Shahab/netmart/RegisterSellerActivity.java
@@ -3,12 +3,19 @@ package com.Shahab.netmart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterSellerActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_seller);
+
+
+
+
     }
 }
