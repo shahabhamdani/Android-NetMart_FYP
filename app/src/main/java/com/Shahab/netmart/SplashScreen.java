52cdf8bb97debc5559c52e01a194650d48407a28
shahabhamdani <shahabhamdani@users.noreply.github.com>
2021-02-21 03:27:41 +0500

    Shahab_Authentication

--- app/src/main/java/com/Shahab/netmart/SplashScreen.java
+++ app/src/main/java/com/Shahab/netmart/SplashScreen.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -29,7 +28,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
 
         //calling splash screen
         splashScreen();
