3631bc3bd85ed4beeb743bbc745a8a19e9cd2232
shahabhamdani <shahabhamdani@users.noreply.github.com>
2021-02-20 16:08:11 +0500

    Minor tweaks

--- app/src/main/java/com/Shahab/netmart/ForgotPasswordActivity.java
+++ app/src/main/java/com/Shahab/netmart/ForgotPasswordActivity.java
@@ -5,16 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+
+
     }
+
+
 
     public void onClickBack(View v){
         startActivity( new Intent(ForgotPasswordActivity.this, LoginActivity.class ));
     }
+
+
+
 }
