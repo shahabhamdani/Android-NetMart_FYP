d29ba2e5a697fbe47146bb8e76327c4ca2078bce
shahabhamdani <shahabhamdani@users.noreply.github.com>
2021-02-22 02:28:33 +0500

    Shahab_Authentication

--- app/src/main/java/com/Shahab/netmart/MainSellerActivity.java
+++ app/src/main/java/com/Shahab/netmart/MainSellerActivity.java
@@ -1,14 +1,206 @@
 package com.Shahab.netmart;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
+
+import java.util.HashMap;
+
+import okhttp3.internal.http2.Header;
 
 public class MainSellerActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navigationView;
+
+    private   TextView userName;
+    private   TextView userPhone;
+    private   TextView userEmail;
+
+    private ImageView cartIv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_seller);
+
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Please Wait");
+            progressDialog.setCanceledOnTouchOutside(false);
+            firebaseAuth = FirebaseAuth.getInstance();
+            checkUser();
+
+        navigationView = (NavigationView) findViewById(R.id.navigation_menu);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Intent intent = null;
+
+                switch (menuItem.getItemId()) {
+                    case R.id.navProfile:
+                        intent = new Intent(MainSellerActivity.this, EditProfileSellerActivity.class);
+                        startActivity(intent);
+                        break;
+
+                    case R.id.navMyCart: {
+                        intent = new Intent(MainSellerActivity.this, CartSellerActivity.class);
+                        startActivity(intent);
+                    }
+                    break;
+
+                    case R.id.navAddProducts: {
+                        intent = new Intent(MainSellerActivity.this, AddProductsSellerActivity.class);
+                        startActivity(intent);
+                    }
+                    break;
+
+                    case R.id.navReviews: {
+                        intent = new Intent(MainSellerActivity.this, ReviewsSellerActivity.class);
+                        startActivity(intent);
+                    }
+                    break;
+
+                    case R.id.navSignOut: {
+                        makeMeOffline();
+                        firebaseAuth.signOut();
+                        checkUser();
+                    }
+                    break;
+
+                }
+                return false;
+            }
+        });
+
+        //Cart Button Click
+        cartIv = findViewById(R.id.cartIv);
+        cartIv.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainSellerActivity.this, CartSellerActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+    private void makeMeOffline() {
+
+        //after logging in, make user online
+        progressDialog.setMessage("Logging Out...");
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("online","false");
+
+        //update value to Database
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        ref.child(firebaseAuth.getUid()).updateChildren(hashMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        //Updated Successfullly
+                        firebaseAuth.signOut();
+
+                        //if logged in Go to Dashboard else go to Login screen
+                        checkUser();
+                    }
+                })
+
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //Failed Updating
+                        progressDialog.dismiss();
+                        Toast.makeText(MainSellerActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+
+    private void checkUser(){
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        if(user == null){
+            startActivity(new Intent(MainSellerActivity.this, LoginActivity.class));
+            finish();
+        }
+        else{
+            loadMyInfo();
+        }
+    }
+
+    private void loadMyInfo() {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        ref.orderByChild("uid").equalTo(firebaseAuth.getUid())
+
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for(DataSnapshot ds: dataSnapshot.getChildren()){
+                            String name = ""+ds.child("name").getValue();
+                            String accountType = ""+ds.child("accountType").getValue();
+                            String phone = ""+ds.child("phone").getValue();
+                            String email = ""+ds.child("email").getValue();
+
+                            userName = (TextView) findViewById(R.id.userNameTv);
+                            userName.setText(name +" ("+accountType+")");
+
+                            userPhone = (TextView) findViewById(R.id.phoneTv);
+                            userPhone.setText(phone);
+
+                            userEmail = (TextView) findViewById(R.id.emailTv);
+                            userEmail.setText(email);
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+    }
+
+
+    public void setUpToolbar() {
+        drawerLayout = findViewById(R.id.drawerLayout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
     }
 }
--- app/src/main/java/com/Shahab/netmart/MainUserActivity.java
+++ app/src/main/java/com/Shahab/netmart/MainUserActivity.java
@@ -6,15 +6,41 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 
 public class MainUserActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+
+    private TextView userName;
+    private TextView userPhone;
+    private TextView userEmail;
+
+    private ImageView cartIv;
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
@@ -24,6 +50,14 @@ public class MainUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_user);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Please Wait");
+        progressDialog.setCanceledOnTouchOutside(false);
+        firebaseAuth = FirebaseAuth.getInstance();
+        checkUser();
+
+
         setUpToolbar();
         navigationView = (NavigationView) findViewById(R.id.navigation_menu);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
@@ -49,8 +83,8 @@ public class MainUserActivity extends AppCompatActivity {
                     break;
 
                     case R.id.navSignOut: {
-                        intent = new Intent(MainUserActivity.this, LoginActivity.class);
-                        startActivity(intent);
+                        makeMeOffline();
+
                     }
                     break;
 
@@ -58,8 +92,94 @@ public class MainUserActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        //Cart Button Click
+        cartIv = findViewById(R.id.cartIv);
+        cartIv.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainUserActivity.this, CartActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
+    private void makeMeOffline() {
+
+        //after logging in, make user online
+        progressDialog.setMessage("Logging Out...");
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("online","false");
+
+        //update value to Database
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        ref.child(firebaseAuth.getUid()).updateChildren(hashMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        //Updated Successfullly
+                        firebaseAuth.signOut();
+
+                        //if logged in Go to Dashboard else go to Login screen
+                        checkUser();
+                    }
+                })
+
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //Failed Updating
+                        progressDialog.dismiss();
+                        Toast.makeText(MainUserActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+
+    private void checkUser(){
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        if(user == null){
+            startActivity(new Intent(MainUserActivity.this, LoginActivity.class));
+            finish();
+        }
+        else{
+            loadMyInfo();
+        }
+    }
+
+    private void loadMyInfo() {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        ref.orderByChild("uid").equalTo(firebaseAuth.getUid())
+
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for(DataSnapshot ds: dataSnapshot.getChildren()){
+                            String name = ""+ds.child("name").getValue();
+                            String accountType = ""+ds.child("accountType").getValue();
+                            String phone = ""+ds.child("phone").getValue();
+                            String email = ""+ds.child("email").getValue();
+
+                            userName = (TextView) findViewById(R.id.userNameTv);
+                            userName.setText(name +" ("+accountType+")");
+
+                            userPhone = (TextView) findViewById(R.id.phoneTv);
+                            userPhone.setText(phone);
+
+                            userEmail = (TextView) findViewById(R.id.emailTv);
+                            userEmail.setText(email);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+    }
     public void setUpToolbar() {
         drawerLayout = findViewById(R.id.drawerLayout);
         Toolbar toolbar = findViewById(R.id.toolbar);
