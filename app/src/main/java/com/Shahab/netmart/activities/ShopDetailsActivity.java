9df8e5b966b3ef85714956a61cdacdb61cb03b12
Maira Naeem <naeemmaira21@gmail.com>
2021-03-29 10:49:55 +0500

    row_product_user.xml activity_shop_deatils

---
+++ app/src/main/java/com/Shahab/netmart/activities/ShopDetailsActivity.java
@@ -0,0 +1,263 @@
+package com.Shahab.netmart.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.Shahab.netmart.Constants;
+import com.Shahab.netmart.R;
+import com.Shahab.netmart.adapters.AdapterProductUser;
+import com.Shahab.netmart.models.ModelProduct;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.security.cert.PolicyNode;
+import java.util.ArrayList;
+
+public class ShopDetailsActivity extends AppCompatActivity {
+
+    //declare ui views
+    private ImageView shopIv;
+    private TextView shopNameTv, phoneTv, emailTv, openCloseTv, deliveryFeeTv, addressTv, filteredProductsTv;
+    private ImageButton cartBtn, backBtn, filterProductBtn;
+    private EditText searchProductEt;
+    private RecyclerView productsRv;
+    private String shopUid;
+    private FirebaseAuth firebaseAuth;
+    private String myLatitude, myLongitude;
+    private String  shopLatitude, shopLongitude, shopName, shopEmail, shopPhone, shopAddress;
+    public String deliveryFee;
+    private ArrayList<ModelProduct> productsList;
+    private AdapterProductUser adapterProductUser;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_details);
+
+        //init ui views
+        shopIv = findViewById(R.id.shopIv);
+        shopNameTv = findViewById(R.id.shopNameTv);
+        phoneTv = findViewById(R.id.phoneTv);
+        emailTv = findViewById(R.id.emailTv);
+        openCloseTv = findViewById(R.id.openCloseTv);
+        deliveryFeeTv = findViewById(R.id.deliveryFeeTv);
+        addressTv = findViewById(R.id.addressTv);
+        cartBtn = findViewById(R.id.cartBtn);
+        backBtn = findViewById(R.id.backBtn);
+        searchProductEt = findViewById(R.id.searchProductEt);
+        filterProductBtn = findViewById(R.id.filterProductBtn);
+        filteredProductsTv = findViewById(R.id.filteredProductsTv);
+        productsRv = findViewById(R.id.productsRv);
+
+        //get uid of the shop from intent
+        shopUid = getIntent().getStringExtra("shopUid");
+        firebaseAuth = FirebaseAuth.getInstance();
+        loadMyInfo();
+        loadShopDetails();
+        loadShopProducts();
+
+        //search
+        searchProductEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try {
+                    adapterProductUser.getFilter().filter(s);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //go previous activity
+                onBackPressed();
+            }
+        });
+
+
+
+        cartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        filterProductBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ShopDetailsActivity.this);
+                builder.setTitle("Filter Products:")
+                        .setItems(Constants.productCategories1, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //get selected item
+                                String selected = Constants.productCategories1[which];
+                                filteredProductsTv.setText(selected);
+                                if (selected.equals("All")){
+                                    //load all
+                                    loadShopProducts();
+                                }
+                                else {
+                                    //load filtered
+                                    adapterProductUser.getFilter().filter(selected);
+                                }
+                            }
+                        })
+                        .show();
+            }
+        });
+
+
+
+
+    }
+
+    private void loadMyInfo() {
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        ref.orderByChild("uid").equalTo(firebaseAuth.getUid())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot ds: dataSnapshot.getChildren()){
+                            //get user data
+                            String name = ""+ds.child("name").getValue();
+                            String email = ""+ds.child("email").getValue();
+                            String myPhone = ""+ds.child("phone").getValue();
+                            String profileImage = ""+ds.child("profileImage").getValue();
+                            String accountType = ""+ds.child("accountType").getValue();
+                            String city = ""+ds.child("city").getValue();
+                            myLatitude = ""+ds.child("latitude").getValue();
+                            myLongitude = ""+ds.child("longitude").getValue();
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+    }
+
+    private void loadShopDetails() {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        ref.child(shopUid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //get shop data
+                String name = ""+dataSnapshot.child("name").getValue();
+                shopName = ""+dataSnapshot.child("shopName").getValue();
+                shopEmail = ""+dataSnapshot.child("email").getValue();
+                shopPhone = ""+dataSnapshot.child("phone").getValue();
+                shopLatitude = ""+dataSnapshot.child("latitude").getValue();
+                shopAddress = ""+dataSnapshot.child("address").getValue();
+                shopLongitude = ""+dataSnapshot.child("longitude").getValue();
+                deliveryFee = ""+dataSnapshot.child("deliveryFee").getValue();
+                String profileImage = ""+dataSnapshot.child("profileImage").getValue();
+                String shopOpen = ""+dataSnapshot.child("shopOpen").getValue();
+
+                //set data
+                shopNameTv.setText(shopName);
+                emailTv.setText(shopEmail);
+                deliveryFeeTv.setText("Delivery Fee: $"+deliveryFee);
+                addressTv.setText(shopAddress);
+                phoneTv.setText(shopPhone);
+                if (shopOpen.equals("true")){
+                    openCloseTv.setText("Open");
+                }
+                else {
+                    openCloseTv.setText("Closed");
+                }
+                try {
+                    Picasso.get().load(profileImage).into(shopIv);
+                }
+                catch (Exception e){
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+
+    private void loadShopProducts() {
+        //init list
+        productsList=new ArrayList<>();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference( "Users");
+        reference.child(shopUid).child("Products")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            //clear list before adding items
+                            productsList.clear();
+                        for (DataSnapshot ds: dataSnapshot.getChildren()){
+                                ModelProduct modelProduct = ds.getValue(ModelProduct.class);
+                                productsList.add(modelProduct);
+
+                            }
+                        //setup adapter
+                        adapterProductUser = new AdapterProductUser(ShopDetailsActivity.this,productsList);
+
+                        //set adapter
+                        productsRv.setAdapter(adapterProductUser);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+
+
+    }
+
+
+
+
+}
